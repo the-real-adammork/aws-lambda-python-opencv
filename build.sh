ae9a43eb0868b69a4fd5513d6a53c5ce1aed17ee
Adam Mork <adam@Adams-MacBook.local>
2017-04-18 19:21:48 -0700

    Updated to support Ubuntu. Added zlib, png, python-dev. Ubuntu AMI - ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20170221 (ami-f4cc1de2)

--- build.sh
+++ build.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Setting up build env
-sudo yum update -y
-sudo yum install -y git cmake gcc-c++ gcc python-devel chrpath
+sudo apt-get update -y
+sudo apt-get install -y git cmake g++ gcc python2.7-dev chrpath libpng-dev zlib1g-dev python-dev
 mkdir -p lambda-package/cv2 build/numpy
 
 # Build numpy
 pip install --install-option="--prefix=$PWD/build/numpy" numpy
-cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
+cp -rf build/numpy/lib/python2.7/site-packages/numpy lambda-package
 
 # Build OpenCV 3.1
 (
-	NUMPY=$PWD/lambda-package/numpy/core/include
-	cd build
-	git clone https://github.com/Itseez/opencv.git
-	cd opencv
-	git checkout 3.1.0
-	cmake										\
-		-D CMAKE_BUILD_TYPE=RELEASE				\
-		-D WITH_TBB=ON							\
-		-D WITH_IPP=ON							\
-		-D WITH_V4L=ON							\
-		-D ENABLE_AVX=ON						\
-		-D ENABLE_SSSE3=ON						\
-		-D ENABLE_SSE41=ON						\
-		-D ENABLE_SSE42=ON						\
-		-D ENABLE_POPCNT=ON						\
-		-D ENABLE_FAST_MATH=ON					\
-		-D BUILD_EXAMPLES=OFF					\
-		-D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"	\
-		.
-	make
+        NUMPY=$PWD/lambda-package/numpy/core/include
+        cd build
+        git clone https://github.com/Itseez/opencv.git
+        cd opencv
+        git checkout 3.1.0
+        cmake                                                                           \
+                -D CMAKE_BUILD_TYPE=RELEASE                             \
+                -D WITH_TBB=ON                                                  \
+                -D WITH_IPP=ON                                                  \
+                -D WITH_V4L=ON                                                  \
+                -D ENABLE_AVX=ON                                                \
+                -D ENABLE_SSSE3=ON                                              \
+                -D ENABLE_SSE41=ON                                              \
+                -D ENABLE_SSE42=ON                                              \
+                -D ENABLE_POPCNT=ON                                             \
+                -D ENABLE_FAST_MATH=ON                                  \
+                -D BUILD_EXAMPLES=OFF                                   \
+                -D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"  \
+                .
+        make
 )
 cp build/opencv/lib/cv2.so lambda-package/cv2/__init__.so
 cp -L build/opencv/lib/*.so.3.1 lambda-package/cv2
